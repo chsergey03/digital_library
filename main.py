ff1d91abc15c4adec0e18d6b396084cc06cbb509
chsergey03 <chsergey1003@gmail.com>
2023-12-16 19:28:09 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from modules.routers import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ modules/routers.py
@@ -0,0 +1,8 @@
+from modules.app import app
+
+from flask import request, render_template
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
